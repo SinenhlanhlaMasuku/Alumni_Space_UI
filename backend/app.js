d98c8f0eb72ebc902d43f5386c6166095920f157
Kfefe23 <kefentsemnisi200@gmail.com>
2023-08-30 14:50:44 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const postgres = require('postgres');
+const { Client } = require('pg');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const port = 3001;
 
+const client = new Client(dbConfig);
+const dbConfig = {
+  host: 'localhost',
+  port: 5432,
+  database: 'pgAdmin',
+  user: 'ppostgres',
+  password: '123',
+};
+
+client.connect()
+
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors());
+
 
 app.post('/api/login', (req, res) => {
   const receivedData = req.body;
@@ -18,6 +32,8 @@ app.post('/api/login', (req, res) => {
 
   // Send a response back to the client
   res.status(200).json({ message: 'Data received on the server', data: receivedData });
+
+  //DATABASE INTERACTION STARTS HERE
 });
 
 app.post('/api/register', (req, res) => {
@@ -31,8 +47,23 @@ app.post('/api/register', (req, res) => {
 
   // Send a response back to the client
   res.status(200).json({ message: 'Data received on the server', data: receivedData });
+
+  //DATABASE INTERACTION STARTS HERE
+  insertDetailsSQL = "INSERT INTO ALUMNI_SPACE_UI(fullname,email,password) " + " VALUES ($1,$2,$3)";
+
+  // Query insert into Alumni_Space_Account
+  client.query(insertDetailsSQL, function (err, result) {
+    if (err) {
+      console.error(err);
+      res.send("An error occurred during registration.");
+    } else {
+      console.log('Registration successful!:');
+
+    }
+  });
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
