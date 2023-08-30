6d73335aa4b0130c61b264d096cfba7590af22aa
Tran203 <tshetshe203@gmail.com>
2023-08-30 10:50:39 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alumni_Space_UI';
+
+  email = '';
+  password = '';
+
+  constructor(private http: HttpClient) {}
+
+  onLogin() {
+    const formData = { name: this.email, surname: this.password };
+    this.http.post('http://localhost:3000/api/login', formData).subscribe((response) => {
+      console.log('Data sent to server:', response);
+      // Clear the form fields after successful submission
+      this.email = '';
+      this.password = '';
+    });
+  }
 }
