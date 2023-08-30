b60f2322663ec854bb90ca425c074693b196e7e8
Tran203 <78562630+Tran203@users.noreply.github.com>
2023-08-30 14:54:50 +0200

    Update script.sql

--- backend/script.sql
+++ backend/script.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Alumni_Space_Account (
+CREATE TABLE ALUMNI_SPACE_UI (
     account_id SERIAL PRIMARY KEY,
     fullname VARCHAR(20) NOT NULL
     email VARCHAR(50) NOT NULL UNIQUE, -- Added UNIQUE constraint
