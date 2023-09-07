276ab0809dbd1738d80c525d646a190b8b99bf84
Tran203 <tshetshe203@gmail.com>
2023-09-07 10:35:23 +0200

    login update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // Add FormsModule
+    HttpClientModule, // Add HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
