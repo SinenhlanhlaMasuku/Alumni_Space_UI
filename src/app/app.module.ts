4e3e92537b2f40e42c2d83d4647ed4eba3ae824f
Tran203 <tshetshe203@gmail.com>
2023-09-08 10:47:33 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     LoginComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
