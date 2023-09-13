c8f4b02b72f4bda0d8ed13645d9bd5f1a1676db2
Tran203 <tshetshe203@gmail.com>
2023-09-13 13:40:00 +0200

    update

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent {
+  email = '';
+
+  constructor(private router: Router) {}
+
+  onForgot() {
+
+    //pass the email
+    localStorage.setItem('email',this.email.toString());
+    this.router.navigate(['/reset-password']);
+  }
 
 }
