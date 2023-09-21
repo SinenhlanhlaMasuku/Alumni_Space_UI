2178d58717de490cbfc10cffee7fa1b6e717947d
Tran203 <tshetshe203@gmail.com>
2023-09-21 10:38:23 +0200

    updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -35,6 +35,38 @@ export class LoginComponent {
     });
   }
 
+  /*onLogin() {
+    const formData = { email: this.email, password: this.password };
+    this.http.post('http://localhost:3000/api/login', formData, { observe: 'response' }).subscribe((response: any) => {
+        console.log('Response from server:', response);
+  
+        // Clear the form fields after submission
+        this.email = '';
+        this.password = '';
+  
+        if (response.status === 200) {
+          // Handle successful login
+          console.log('User logged in as:', response.body.result[0].name);
+          localStorage.setItem('name', response.body.result.name);
+          this.router.navigate(['/homepage']);
+        } else if (response.status === 401) {
+          // Handle invalid login
+          console.log('Invalid email or password');
+          this.router.navigate(['/forgot-password']);
+        } else {
+          // Handle other errors
+          console.log('An error occurred during login:', response.statusText);
+          // You can add additional error handling logic here if needed
+        }
+      },
+      (error) => {
+        // Handle network or other errors
+        console.error('An error occurred:', error);
+        // You can add specific error handling logic here if needed
+      }
+    );
+  } */
+
   onSubmit() {
     const formData = { fullname: this.fullname,surname: this.surname, email: this.email, password: this.password };
     this.http.post('http://localhost:3000/api/register', formData).subscribe((response: any) => {
