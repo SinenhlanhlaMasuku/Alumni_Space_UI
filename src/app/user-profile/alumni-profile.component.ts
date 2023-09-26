506f7a1473472c243931eb33cf800615f68852aa
Sihle-Mhlongo <143382589+Sihle-Mhlongo@users.noreply.github.com>
2023-09-26 06:12:31 +0200

    new files

---
+++ src/app/user-profile/alumni-profile.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumni-profile',
+  templateUrl: './alumni-profile.component.html',
+  styleUrls: ['./alumni-profile.component.css']
+})
+export class AlumniProfileComponent {
+  title='Alumni_Profile'
+  username= ''
+ userEmail=''
+  userlocation=''
+  userQualification=''
+  userSkills='----'
+  userExperience=''
+  Employment_Status=''
+  Academic_Transcript=''
+  userInterest =''
+  userprofilePic=''
+  
+
+ editProfile(){
+
+  
+ }
+  
+    
+ 
+}
