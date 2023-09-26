27202641839d7a1e3d5098d4c90c5912d7904227
Sihle-Mhlongo <143382589+Sihle-Mhlongo@users.noreply.github.com>
2023-09-26 06:15:08 +0200

    Add files via upload

---
+++ src/app/editprofile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editprofile',
+  templateUrl: './editprofile.component.html',
+  styleUrls: ['./editprofile.component.css']
+})
+export class EditprofileComponent {
+  name =''
+
+}
