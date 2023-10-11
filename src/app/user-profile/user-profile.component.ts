a2e80a02ac6aea2dd8d975dc433f148648496c30
Sihle-Mhlongo <sihlemhlongo475@gmail.com>
2023-10-11 06:27:25 +0200

    admin home and userprofile changes

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
+  user = [{
+      //  name: string,
+         location: [],
+         Qualification: [],
+       Skills: [],
+        Experience: [],
+          Employment_Status: []
 
+  }]
+  uploadSuccess(){}
+  uploadError(){}
 }
