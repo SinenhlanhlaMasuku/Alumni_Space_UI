d67228756403cb4bfff0bbb89685ca1c54499abd
Tran203 <tshetshe203@gmail.com>
2023-10-11 10:36:20 +0200

    update

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -8,8 +8,19 @@ import { Component } from '@angular/core';
 export class UserProfileComponent {
 
 profile = {
-  
+  name:'',
+  email:''
 }
+
+
+  saveProfile(){
+    const formData = { name: this.profile.name, email: this.profile.email};
+
+    //set name
+
+    console.log(formData);
+    
+  }
 
 
 }
