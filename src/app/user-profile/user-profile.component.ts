fdc50aaebf0588026914b875f080f51108193b45
SinenhlanhlaMasuku <118171418+SinenhlanhlaMasuku@users.noreply.github.com>
2023-10-11 09:35:14 +0200

    user-profile update

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
@@ -17,4 +17,49 @@ export class UserProfileComponent {
   }]
   uploadSuccess(){}
   uploadError(){}
+}*/
+// userProfileController.ts
+
+class UserProfileController {
+  user: any;
+  isProfile: boolean;
+  isEditing: boolean;
+  uploadSuccess: string;
+  uploadError: string;
+
+  constructor() {
+    this.user = {
+      name: "Alumni name",
+      email: "alumni@example.com",
+      location: "Location",
+      Qualification: "Qualification",
+      Skills: "Skills",
+      Experience: "Experience",
+      Employment_Status: "Employed",
+      Interest: "Interests",
+    };
+
+    this.isProfile = true;
+    this.isEditing = false;
+    this.uploadSuccess = "";
+    this.uploadError = "";
+  }
+
+  editProfile() {
+    this.isProfile = false;
+    this.isEditing = true;
+  }
+
+  saveProfile() {
+    this.isProfile = true;
+    this.isEditing = false;
+    // Update user profile data to the server here
+  }
+
+  uploadFile() {
+    // Handle file uploads here
+    // Update this.uploadSuccess or this.uploadError based on the success or error of the upload
+  }
 }
+
+angular.module("userProfileApp").controller("UserProfileController", UserProfileController);
