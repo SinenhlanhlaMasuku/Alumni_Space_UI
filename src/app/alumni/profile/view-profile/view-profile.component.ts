56e0c9675f40af559b87c49f3884b8f1bcd40033
Tran203 <tshetshe203@gmail.com>
2023-10-13 12:04:20 +0200

    Update view-profile.component.ts

--- src/app/alumni/profile/view-profile/view-profile.component.ts
+++ src/app/alumni/profile/view-profile/view-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 //import { UserProfileService } from '../user-profile.service';
 
 @Component({
@@ -18,6 +20,33 @@ export class ViewProfileComponent {
           Interest: "Interest placeholder",
           Bio : "Bio placeholder",
   }
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit() {
+    const storedName = localStorage.getItem('name');
+    const user_id = localStorage.getItem('account_id');
+
+    console.log('User_id:' + user_id);
+
+    //get profile details from server
+    this.http.put('http://localhost:3000/api/userprofile', {user_id}).subscribe((response: any) => {
+      console.log('Data sent to server:', response);
+
+      //this.alumni.Skills = response.userprofile.skills;
+      console.log( response.result[0].skills);
+        this.alumni.Skills = response.result[0].skills;
+        this.alumni.Experience = response.result[0].experience;
+    });
+
+    
+    if (storedName) {
+      // Update the 'name' property if 'name' is found in localStorage
+      this.alumni.Name = storedName;
+    }
+  }
+
+
   /*constructor(@Inject(UserProfileService) private service : UserProfileService){
   }
   editProfile(view: HTMLButtonElement){
