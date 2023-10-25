762176e8903dc34163e678e056bdba2c7ecb1e57
Tran203 <tshetshe203@gmail.com>
2023-10-25 15:15:44 +0200

    update folder structured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//import individual components
+import { IndexComponent } from './components/index/index.component';
+import { ServiceComponent } from './components/service/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+//
+
+const routes: Routes = [
+  {path:'',component:IndexComponent},
+  {path:'',component:ServiceComponent},
+  {path:'',component:AboutComponent},
+  {path:'',component:ContactComponent},
+
+  //Call Alumni module
+
+  //Call Admni module
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//indiviual components
+import { IndexComponent } from './components/index/index.component';
+import { ServiceComponent } from './components/service/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ServiceComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
