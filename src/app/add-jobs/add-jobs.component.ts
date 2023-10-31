f89b80e9a08c4c21476b8e094aff3a08a6eb7847
Tran203 <tshetshe203@gmail.com>
2023-10-31 10:44:54 +0200

    updated add-job page

--- src/app/add-jobs/add-jobs.component.ts
+++ src/app/add-jobs/add-jobs.component.ts
@@ -8,34 +8,155 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-jobs.component.css']
 })
 export class AddJobsComponent {
+  StudentArray : any[] = [ ];
+  isResultLoaded = false;
+  isUpdateFormActive = false;
 
-  title = 'Alumni_Space_UI';
+  num : number=0;
+  
+  id = '';
+  job_title : string= ' ';
+  Organisation : string= '';
+  workplace_type: string= '';
+  location : string= '';
+  job_type : string= '';
+  job_description : string= '';
+  date_posted : string= '';
+  deadline : string= '';
 
-  job_title = '';
-  Organisation = '';
-  workplace_type = '';
-  location = '';
-  job_type = '';
-  job_description = '';
-  date_posted = '';
-  deadline = '';
+ /* stname: string ="";
+  course: string ="";
+  fee: string ="";
+  currentStudentID = "";*/
 
-  constructor(private http: HttpClient, private router: Router) {}
-  postJob(){
-    const formData = { job_title: this.job_title, Organisation: this.Organisation,  workplace_type: this.workplace_type, location:this.location, job_type:this.job_type, job_description:this.job_description, date_posted:this.date_posted, deadline:this.deadline};
-    this.http.post('http://localhost:3000/api/newjob', formData).subscribe((response: any) => {
+  constructor(private http: HttpClient ) 
+  {
+    this.getAllStudent();
+  }
+
+  ngOnInit() {
+    this.http.get('http://localhost:3000/api/jobs' ).subscribe((response: any) => {
       console.log('Data sent to server:', response);
-      // Clear the form fields after successful submission
-      this.job_title = '';
-      this.Organisation = '';
-      this.workplace_type = '';
-      this.location = '';
-      this.job_type = '';
-      this.job_description = '';
-      this.date_posted = '';
-      this.deadline = '';
+    // Fetch jobs using the service
+    this.StudentArray = response.jobs;
+   this.num = this.StudentArray.length;
+  });
+  }
 
-      //this.router.navigate(['/Job Added successful']);
+  getAllStudent()
+  { 
+    this.http.get("http://localhost:3000/api/newjob")
+    .subscribe((resultData: any)=>
+    {
+        this.isResultLoaded = true;
+        console.log(resultData.data);
+        this.StudentArray = resultData.data;
+        console.log(this.StudentArray);
     });
   }
+
+  
+
+  
+  
+
+
+  register()
+  {
+   // this.isLogin = false; 
+   // alert("hi");
+    let bodyData = {
+
+  "job_title" : this.job_title,
+  "Organisation" : this.Organisation,
+  "workplace_type" : this.workplace_type,
+  "location" : this.location,
+  "job_type" : this.job_type,
+  "job_description" : this.job_description,
+  "date_posted" : this.date_posted,
+  "deadline" : this.deadline,
+     
+    };
+
+    this.http.post("http://localhost:3000/api/newjob",bodyData).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Job Added Successfully");
+        this.getAllStudent();
+      //  this.name = '';
+      //  this.address = '';
+      //  this.mobile  = 0;
+     
+     
+    });
+  }
+
+  setUpdate(data: any) 
+  {
+   
+   this.job_title = data.job_title;
+   this.Organisation = data.Organisation;
+   this.workplace_type = data.workplace_type;
+   this.location = data.location;
+   this.job_type = data.job_type;
+   this.job_description = data.job_description;
+   this.date_posted = data.date_posted;
+   this.deadline = data.deadline;
+  }
+
+  UpdateRecords()
+  {
+    let bodyData = 
+    {
+      /*"stname" : this.stname,
+      "course" : this.course,
+      "fee" : this.fee*/
+      "job_title" : this.job_title,
+  "Organisation" : this.Organisation,
+  "workplace_type" : this.workplace_type,
+  "location" : this.location,
+  "job_type" : this.job_type,
+  "job_description" : this.job_description,
+  "date_posted" : this.date_posted,
+  "deadline" : this.deadline,
+    };
+    
+    this.http.put("http://localhost:3000/api/Jobs/:job_id"+ "/"+ this.id,bodyData).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Job Updateddd")
+        this.getAllStudent();
+      
+    });
+  }
+ 
+  save()
+  {
+    if(this.id == '')
+    {
+        this.register();
+    }
+      else
+      {
+       this.UpdateRecords();
+      }       
+
+  }
+
+
+  setDelete(data :any)
+  {
+    //get job id
+    const job_id =  data.job_id;
+    console.log(job_id);
+    //
+    const url = "http://localhost:3000/api/job/delete" + "/" + job_id;
+    this.http.delete(url).subscribe((resultData: any) => {
+        console.log(resultData);
+        alert("Job Deletedddd")
+        this.getAllStudent();
+    });
+  }
+
+
 }
