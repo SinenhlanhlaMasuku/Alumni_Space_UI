91255b4450ac3c58b4d058ed1e05666f630008c9
SinenhlanhlaMasuku <118171418+SinenhlanhlaMasuku@users.noreply.github.com>
2023-11-01 14:56:56 +0200

    posts updates

--- src/app/posts/your-thoughts/your-thoughts.component.ts
+++ src/app/posts/your-thoughts/your-thoughts.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./your-thoughts.component.css']
 })
 export class YourThoughtsComponent {
+  // Functions for the functionalities
+  uploadPhoto(): void {
+    // Handle photo upload functionality
+    console.log('Upload photo');
+  }
 
+  uploadVideo(): void {
+    // Handle video upload functionality
+    console.log('Upload video');
+  }
+
+  createEvent(): void {
+    // Handle event creation functionality
+    console.log('Create event');
+  }
+
+  expressFeelingActivity(): void {
+    // Handle feeling/activity expression functionality
+    console.log('Express feeling/activity');
+  }
+
+  createPoll(): void {
+    // Handle create a poll functionality
+    console.log('Create a poll');
+  }
+
+  askQuestion(): void {
+    // Handle asking a question functionality
+    console.log('Ask a question');
+  }
+
+  help(): void {
+    // Handle help functionality
+    console.log('Help');
+  }
 }
