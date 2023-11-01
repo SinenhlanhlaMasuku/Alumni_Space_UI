2fecd6fe1ddc3d0f8e66bec8dce2cd3ce45d4ebf
SinenhlanhlaMasuku <118171418+SinenhlanhlaMasuku@users.noreply.github.com>
2023-11-01 10:29:34 +0200

    alumni Landing page updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,27 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { AlumniStatsComponent } from './alumni-stats/alumni-stats.component';
-import { EventsStatsComponent } from './events-stats/events-stats.component';
-import { JobsStatsComponent } from './jobs-stats/jobs-stats.component';
-import { LastFewNotificationsComponent } from './last-few-notifications/last-few-notifications.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
+import { UserLandingPageComponent } from 'src/app/posts/user-landing-page/user-landing-page.component';
+// import { AdminHomeComponent } from './admin-home/admin-home.component';
+// import { UserProfileComponent } from './user-profile/user-profile.component';
+// import { AlumniStatsComponent } from './alumni-stats/alumni-stats.component';
+// import { EventsStatsComponent } from './events-stats/events-stats.component';
+// import { JobsStatsComponent } from './jobs-stats/jobs-stats.component';
+// import { LastFewNotificationsComponent } from './last-few-notifications/last-few-notifications.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+// import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'homepage', component: HomepageComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
-  {path: 'adminHome', component: AdminHomeComponent},
-  {path: 'alumniProfile', component: UserProfileComponent},
-  {path: 'alumniStats', component: AlumniStatsComponent},
-  {path: 'eventsStats', component: EventsStatsComponent},
-  {path: 'jobsStats', component: JobsStatsComponent} ,
-  {path: 'lastfewnotifications', component: LastFewNotificationsComponent},
-  {path: 'navbar', component: NavbarComponent},
-  {path: 'topWidget', component: TopWidgetsComponent}
+  {path: 'index',component:UserLandingPageComponent}
+  // {path: 'adminHome', component: AdminHomeComponent},
+  // {path: 'alumniProfile', component: UserProfileComponent},
+  // {path: 'alumniStats', component: AlumniStatsComponent},
+  // {path: 'eventsStats', component: EventsStatsComponent},
+  // {path: 'jobsStats', component: JobsStatsComponent} ,
+  // {path: 'lastfewnotifications', component: LastFewNotificationsComponent},
+  // {path: 'navbar', component: NavbarComponent},
+  // {path: 'topWidget', component: TopWidgetsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+//import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { AppRoutingModule } from './app-routing.module';
@@ -8,15 +8,14 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { ChartModule } from 'angular-highcharts';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
-import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
-import { AlumniStatsComponent } from './alumni-stats/alumni-stats.component';
-import { JobsStatsComponent } from './jobs-stats/jobs-stats.component';
-import { EventsStatsComponent } from './events-stats/events-stats.component';
-import { LastFewNotificationsComponent } from './last-few-notifications/last-few-notifications.component';
+import { PeopleYouMayKnowComponent } from './posts/people-you-may-know/people-you-may-know.component';
+import { UserLandingPageComponent } from './posts/user-landing-page/user-landing-page.component';
+import { UserNavbarComponent } from './posts/user-navbar/user-navbar.component';
+import { UserPostComponent } from './posts/user-post/user-post.component';
+import { UserProfileInfoComponent } from './posts/user-profile-info/user-profile-info.component';
+import { UserStorysComponent } from './posts/user-storys/user-storys.component';
+import { WhoToFollowComponent } from './posts/who-to-follow/who-to-follow.component';
+import { YourThoughtsComponent } from './posts/your-thoughts/your-thoughts.component';
 // import {  } from '@fortawesome/fontawesome-free-solid';
 
 
@@ -28,14 +27,14 @@ import { LastFewNotificationsComponent } from './last-few-notifications/last-few
     HomepageComponent,
     LoginComponent,
     ForgotPasswordComponent,
-    UserProfileComponent,
-    NavbarComponent,
-    AdminHomeComponent,
-    TopWidgetsComponent,
-    AlumniStatsComponent,
-    JobsStatsComponent,
-    EventsStatsComponent,
-    LastFewNotificationsComponent,
+    PeopleYouMayKnowComponent,
+    UserLandingPageComponent,
+    UserNavbarComponent,
+    UserPostComponent,
+    UserProfileInfoComponent,
+    UserStorysComponent,
+    WhoToFollowComponent,
+    YourThoughtsComponent
     
 
 
@@ -43,11 +42,11 @@ import { LastFewNotificationsComponent } from './last-few-notifications/last-few
     imports: [
     BrowserModule,
     AppRoutingModule,
-     FontAwesomeModule,
+    
      HttpClientModule,// Add HttpClientModule
-     FontAwesomeModule,
+   
      FormsModule,
-     ChartModule,
+    
     //  AuthorsComponent
     
   ],
