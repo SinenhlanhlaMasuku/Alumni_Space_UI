2f508beec4dece17bc378ec7b8fc10c0409b64c4
Tran203 <tshetshe203@gmail.com>
2023-11-03 13:51:42 +0200

    Update profile.service.ts

--- src/app/modules/alumni/profile/profile.service.ts
+++ src/app/modules/alumni/profile/profile.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  //constructor() { }
+  constructor(private http: HttpClient) { }
 
   private documents: File[] = [];
+  private apiUrl = 'http://localhost:3000/api/upload';
 
   uploadDocument(document: File) {
     this.documents.push(document);
+
+    const formData = new FormData();
+    formData.append('file_name', document);
+    formData.append('fileType', 'academic');
+
+    console.log(formData);
+    
+    return this.http.post(this.apiUrl, formData);
   }
 
   getDocuments() {
