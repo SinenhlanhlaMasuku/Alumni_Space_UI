d2fbd5830ee2f54e0fc8055b39085b40f7bead80
SinenhlanhlaMasuku <118171418+SinenhlanhlaMasuku@users.noreply.github.com>
2023-11-03 11:22:08 +0200

    updates

--- src/app/posts/user-navbar/user-navbar.component.ts
+++ src/app/posts/user-navbar/user-navbar.component.ts
@@ -8,3 +8,35 @@ import { Component } from '@angular/core';
 export class UserNavbarComponent {
 
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const modeSwitchButtons = document.querySelectorAll('.btn-modeswitch');
+  const html = document.querySelector('html');
+
+  if (html) {
+      modeSwitchButtons.forEach((button) => {
+          button.addEventListener('click', (event) => {
+              const themeValue = button.getAttribute('data-bs-theme-value');
+
+              if (themeValue === 'light') {
+                  html.setAttribute('data-theme', 'light');
+              } else if (themeValue === 'dark') {
+                  html.setAttribute('data-theme', 'dark');
+              } else if (themeValue === 'auto') {
+                  html.removeAttribute('data-theme');
+              }
+
+              // Store the theme preference in localStorage
+              if (themeValue) {
+                  localStorage.setItem('theme', themeValue);
+              }
+          });
+      });
+
+      // Check for a stored theme preference in localStorage
+      const storedTheme = localStorage.getItem('theme');
+      if (storedTheme) {
+          html.setAttribute('data-theme', storedTheme);
+      }
+  }
+});
+
