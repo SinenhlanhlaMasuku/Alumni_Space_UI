023c64303b944bf1768135a5913c30ae5dd59617
Sihle-Mhlongo <sihlemhlongo475@gmail.com>
2023-11-06 05:51:27 +0200

    alumnistats fjxed

--- src/app/admin-footer/admin-footer.component.ts
+++ src/app/admin-footer/admin-footer.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+// import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'; // Import the specific icons
+//  import{ faFacebook, faTwitter} from '@fortawesome/fontawesome-svg-core';
+// import { from } from 'rxjs';
 
 @Component({
   selector: 'app-admin-footer',
@@ -6,5 +10,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-footer.component.css']
 })
 export class AdminFooterComponent {
-
+  // faFacebook = faFacebook; // Assign the imported icons to class properties
+  // faTwitter = faTwitter;
 }
--- src/app/homepage/Admin-home/alumni-stats/alumni-stats.component.ts
+++ src/app/homepage/Admin-home/alumni-stats/alumni-stats.component.ts
@@ -7,107 +7,93 @@ import { Chart } from 'angular-highcharts';
   styleUrls: ['./alumni-stats.component.css']
 })
 export class AlumniStatsComponent {
-  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   backgroundColor = [
     'rgba(255, 99, 132, 1)',
     'rgba(54, 162, 235, 1)',
     'rgba(255, 205, 86, 1)',
     'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',//5
+    'rgba(153, 102, 255, 1)',
     'rgba(176, 83, 25, 1)',
     'rgba(94, 190, 250, 1)',
     'rgba(225, 27, 80, 1)',
     'rgba(22, 1, 192, 1)',
-    'rgba(159, 122, 234. 1)',//10
+    'rgba(159, 122, 234, 1)',
     'rgba(70, 200, 72, 1)',
-    'rgba(252, 3, 7, 1)',//12
-
-
-
+    'rgba(252, 3, 7, 1)',
   ];
-  data = [12, 19, 3, 5, 2, 4, 8, 34, 36, 26, 45, 8];
 
-  chart = new Chart({
-    chart: {
-      type: 'column',
-      height: 250,
-      width: 350,
-      
-    },
-    title: {
-      text: 'Alumni stats'
-    },
-    xAxis: {
-     
-      categories: this.months,
-      title: {
-        text: 'Months'
-      }
-    },
-    yAxis: {
-       title: {
-        text: 'Number of Alumnis'
-      }
-    },
-    series: [
-      {
-        name: "Alumni(s)",
-        type: "column",
-        showInLegend: false,
-        data: [
-          {
-            color: this.backgroundColor[6],
-            y: this.data[0],
-          },
-          {
-            color: this.backgroundColor[8],
-            y: this.data[1]
-          },
-          {
-            color: this.backgroundColor[6],
-            y: this.data[2]
-          },
-          {
-            color: this.backgroundColor[8],
-            y: this.data[3]
-          },
-          {
-            color: this.backgroundColor[6],
-            y: this.data[4],
-          },
-          {
-            color: this.backgroundColor[8],
-            y: this.data[5]
-          },
-          {
-            color: this.backgroundColor[6],
-            y: this.data[6]
-          },
-          {
-            color: this.backgroundColor[8],
-            y: this.data[7]
-          },
-          {
-            color: this.backgroundColor[6],
-            y: this.data[8],
-          },
-          {
-            color: this.backgroundColor[8],
-            y: this.data[9]
-          },
-          {
-            color: this.backgroundColor[6],
-            y: this.data[10]
-          },
-          {
-            color: this.backgroundColor[8],
-            y: this.data[11]
-          },
-        ],
+  data: { [key: string]: number[] } = {
+    '2023': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // Example data for the year 2023
+    '2024': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // Example data for the year 2024
+    // Add data for other years as needed
+  };
+
+  chart!: Chart;
+
+  constructor() {
+    this.renderChart();
+  }
+
+  renderChart() {
+    const currentYear = new Date().getFullYear().toString();
+    const validatedData = this.validateAlumniData(currentYear);
+    this.chart = new Chart({
+      chart: {
+        type: 'column',
+        height: 250,
+        width: 350,
       },
-    ],
-    credits: {
-      enabled: false,
+      title: {
+        text: 'Alumni stats'
+      },
+      xAxis: {
+        categories: this.getMonths(currentYear),
+        title: {
+          text: 'Months of registration'
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Number of Alumni registered'
+        }
+      },
+      series: [
+        {
+          name: 'Alumni(s)',
+          type: 'column',
+          showInLegend: false,
+          data: validatedData.map((value, index) => {
+            return {
+              color: this.backgroundColor[index % this.backgroundColor.length],
+              y: value,
+            };
+          }),
+        },
+      ],
+      credits: {
+        enabled: false,
+      }
+    });
+  }
+
+  validateAlumniData(year: string): number[] {
+    const currentYear = new Date().getFullYear().toString();
+    const currentMonth = new Date().getMonth();
+    const isCurrentYear = currentYear === year;
+    const data = this.data[year] || [];
+
+    if (isCurrentYear) {
+      return data.slice(0, currentMonth + 1);
+    } else {
+      return data;
     }
-  })
+  }
+
+  getMonths(year: string): string[] {
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    return months.map(month => month + ' ' + year);
+    // + year
+  }
 }
+
+
