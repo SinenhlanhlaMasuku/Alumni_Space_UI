4018fe4baebbbf9dc30e8cae434caf15a16a8317
Tran203 <tshetshe203@gmail.com>
2023-11-27 15:20:52 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,4 @@
 // config.ts
 export const baseUrl = 'http://192.168.27.20:3000/api';
 export const chatUrl = 'http://192.168.27.20:3001';
-<<<<<<< Updated upstream
 export const imageUrl = 'http://192.168.27.20:3000';
-=======
->>>>>>> Stashed changes
