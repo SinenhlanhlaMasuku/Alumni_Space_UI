203615951797bdb98224958aa557b7da4c3b0718
Sihle-Mhlongo <sihlemhlongo475@gmail.com>
2023-11-27 14:32:12 +0200

    untrusted

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 // config.ts
-export const baseUrl = 'http://localhost:3000/api';
-export const chatUrl = 'http://localhost:3001';
-export const imageUrl = 'http://localhost:3000';
+export const baseUrl = 'http://192.168.27.20:3000/api';
+export const chatUrl = 'http://192.168.27.20:3001';
+export const imageUrl = 'http://192.168.27.20:3000';
