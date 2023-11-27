e950b377fb655243b1aa531787cc4efd08df9d92
Sihle-Mhlongo <sihlemhlongo475@gmail.com>
2023-11-27 14:32:34 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,7 @@
 // config.ts
 export const baseUrl = 'http://192.168.27.20:3000/api';
 export const chatUrl = 'http://192.168.27.20:3001';
+<<<<<<< Updated upstream
 export const imageUrl = 'http://192.168.27.20:3000';
+=======
+>>>>>>> Stashed changes
