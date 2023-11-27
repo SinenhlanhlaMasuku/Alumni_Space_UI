e2f50c7761850a0e761d7c2887c182a223bfaecb
Tran203 <tshetshe203@gmail.com>
2023-11-27 16:09:00 +0200

    update view-resume

--- src/app/modules/admin/job-applications/alumni-resume/alumni-resume.component.ts
+++ src/app/modules/admin/job-applications/alumni-resume/alumni-resume.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
-import {MatDividerModule} from '@angular/material/divider';
-import{MatGridListModule} from '@angular/material/grid-list';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ProfileService } from 'src/app/modules/alumni/profile/profile.service';
+
+import { imageUrl } from 'config';
+
 interface Alumni {
   Bio: string;
   name: string;
   surname: string;
   skills: string[];
-  address: string;
+  location: string;
   email: string;
   experience: string;
   qualification: string;
@@ -26,29 +30,45 @@ interface Alumni {
 
 
 export class AlumniResumeComponent {
-    //  alumni_fullName = "Sihle Mhlongo";
-    //  alumni: string[] =[{
-    //     fullNames: '',
-    //  }]
-    // certificates: string[]=[];
-    @Input() alumni: Alumni | undefined;
-    // certificates: string[] = [ 'assets/certificate1.pdf',
-    //     'assets/certificate2.pdf',
-    //   'assets/certifacate3.pdf']
-    //   certificateNames: string[] = ['Certificate 1', ' Certificate 2', 'certificate 3'];
+  //  alumni_fullName = "Sihle Mhlongo";
+  //  alumni: string[] =[{
+  //     fullNames: '',
+  //  }]
+  // certificates: string[]=[];
+  @Input() alumni: Alumni | undefined;
+  // certificates: string[] = [ 'assets/certificate1.pdf',
+  //     'assets/certificate2.pdf',
+  //   'assets/certifacate3.pdf']
+  //   certificateNames: string[] = ['Certificate 1', ' Certificate 2', 'certificate 3'];
 
-    //  alumni: Alumni[] = [
-    //   {
-    //     fullNames: 'Sihle Bandile',
-    //     lastName: 'Mhlongo',
-    //     Bio: 'I am a highly motivated professional with a passion for [your industry or field]. With a solid background in [mention relevant skills or experiences], I bring a unique blend of [specific qualities] that drive successful outcomes',
-    //     address: '245 burger str',
-    //     skills: ['JavaScript', 'Angular', 'Node.js'],
-    //     email: 'john.doe@example.com',
-    //     experience: '5 years',
-    //     qualification: 'Bachelor of Science in Computer Science',
-    //     interests: ['coding', ' gaming'],
-    //   }
-    //   ];
+  //  alumni: Alumni[] = [
+  //   {
+  //     fullNames: 'Sihle Bandile',
+  //     lastName: 'Mhlongo',
+  //     Bio: 'I am a highly motivated professional with a passion for [your industry or field]. With a solid background in [mention relevant skills or experiences], I bring a unique blend of [specific qualities] that drive successful outcomes',
+  //     address: '245 burger str',
+  //     skills: ['JavaScript', 'Angular', 'Node.js'],
+  //     email: 'john.doe@example.com',
+  //     experience: '5 years',
+  //     qualification: 'Bachelor of Science in Computer Science',
+  //     interests: ['coding', ' gaming'],
+  //   }
+  //   ];
+
+  constructor(private ProfileService: ProfileService) {
+  }
+
+  private url = `${imageUrl}/uploads/pics/profiles`
+
+  getAlumniPicturePath(picFile: string  | undefined): string {
+    const defaultPicture = 'default-avatar.jpg';
+  
+    if (picFile && picFile.trim() !== '') {
+      return `${this.url}/${picFile}`;
+    } else {
+      return `${this.url}/${defaultPicture}`;
+    }
+  }
+  
 
 }
