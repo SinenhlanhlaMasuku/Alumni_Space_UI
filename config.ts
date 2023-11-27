6cefd26bf0178aec8f680531a9f04d6fe0cc6588
Tran203 <tshetshe203@gmail.com>
2023-11-27 16:45:36 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,10 @@
 // config.ts
-export const baseUrl = 'http://192.168.27.20:3000/api';
-export const chatUrl = 'http://192.168.27.20:3001';
-export const imageUrl = 'http://192.168.27.20:3000';
+
+//server ip address
+ const host = 'localhost';
+
+
+//export const baseUrl = 'http://192.168.27.20:3000/api';
+export const baseUrl = 'http://' + host + ':3000/api';
+export const chatUrl = 'http://' + host + ':3001';
+export const imageUrl = 'http://' + host + ':3000';
