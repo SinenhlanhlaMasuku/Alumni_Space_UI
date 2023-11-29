e5cb59da026c5538facebbc83a0b77d7b1a9b5bc
Tran203 <tshetshe203@gmail.com>
2023-11-29 15:44:53 +0200

    updated track-my-application

--- config.ts
+++ config.ts
@@ -4,7 +4,8 @@
  const host = '192.168.27.20';
 
 
+
 //export const baseUrl = 'http://192.168.27.20:3000/api';
-export const baseUrl = 'http://192.168.27.20:3000/api';
-export const chatUrl = 'http://192.168.27.20:3001';
-export const imageUrl = 'http://192.168.27.20:3000';
+export const baseUrl = 'http://localhost:3000/api';
+export const chatUrl = 'http://192.168.27.16:3001';
+export const imageUrl = 'http://192.168.27.16:3000';
--- src/app/modules/alumni/tract-application/tract-application.component.ts
+++ src/app/modules/alumni/tract-application/tract-application.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { JobAppService } from 'src/app/services/jobApp/job-app.service';
 interface Alumni{
-  job_title: string;
+  saved_job_title: string;
   application_status: string;
   application_date: string; 
+  timeStamp?: Date;
 }
 @Component({
   selector: 'app-tract-application',
@@ -11,49 +12,40 @@ interface Alumni{
   styleUrls: ['./tract-application.component.css']
 })
 export class TractApplicationComponent {
-  timeReplied: string = '';
-  Repliedtime?: Date;
-  appData: any[] = []; 
-  responseForms: boolean[] = new Array(this.appData.length).fill(false);
+  appData: Alumni[] = [];
 
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private jobAppService: JobAppService
+  ) {}
 
-  constructor(private changeDetectorRef: ChangeDetectorRef, private jobAppService: JobAppService){}
+  ngOnInit() {
+    this.fetchDataFromApi();
+  }
 
-  ngOnInit(){
-  
+  fetchDataFromApi() {
     this.jobAppService.getApplications().subscribe(
-      (data) => {
+      (data: any) => {
         this.appData = data.jobs;
+        console.log(this.appData);
+        
       },
       (error) => {
         console.error('Error fetching alumni data:', error);
       }
     );
-
-    this.fetchDataFromApi();
-  }
-  
-  
-
-  fetchDataFromApi() {
-    this.jobAppService.getApplications().subscribe(data => {
-      this.appData = data;
-    });
   }
 
-  toggleResponseForm(index: number) {
-    this.responseForms[index] = !this.responseForms[index];
+  trackByFn(index: number, item: Alumni) {
+    return item.saved_job_title; 
+  
   }
 
-  submitResponse(index: number) {
-    // this.data[index].status = 'Answered' + ' ' + this.timeReplied;
-    // this.responseForms[index] = false;
-
-    const currentTime = new Date();
-  
-
-  this.appData[index].status = 'Answered';
-  this.appData[index].timeStamp = new Date();
-  this.responseForms[index] = true;
+  toggleResponseForm(row: Alumni) {
+    
+    const index = this.appData.indexOf(row);
+    if (index !== -1) {
+      this.appData.splice(index, 1);
+    }
   }
 }
